29f89fcba02a0c1491030d7dc47953275cc5b8e0
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-19 16:44:34 +0200

    Add files via upload

---
+++ php/getHistory.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+
+	$username = "s1965919";
+	$password = "ICTPass1670";
+	$database = "d1965919";
+	$link = mysqli_connect("127.0.0.1", $username, $password, $database);
+	$output = array(); //stores all the items from different catogories
+	$output1 = array();
+	$output2 = array();
+	
+	
+		
+	$studentNo = $_SESSION['login_user'];
+	if ($pResult = mysqli_query($link, "select MARKET_ID, PURCHASE_DATE from PURCHASES where STUDENT_NO='$studentNo';")) {
+	$length = 0;
+	while ($row = $pResult->fetch_assoc()) {
+	$output1[] = $row;
+	++$length;
+	}
+	
+	for ($x = 0; $x < $length; $x++) {
+	$data = $output1[$x]['MARKET_ID'];
+	$date = $output1[$x]['PURCHASE_DATE'];
+	if ($mResult = mysqli_query($link, "select * from MARKET_NEW where MARKET_ID='$data';")) {
+	while ($row = $mResult->fetch_assoc()) {
+	$output2[] = $row;
+	//array_push($output2, $date)
+	}
+	}
+	}
+	array_push($output, $output1, $output2);
+	echo json_encode($output);
+	}
+	else
+	echo "failed";
+	
+	/*
+	if ($result = mysqli_query($link, "select * from MARKET_NEW")) {
+	while ($row = $result->fetch_assoc()) {
+	$output[] = $row;
+	}
+	echo json_encode($output);
+	}
+	else
+	echo json_encode("failed");*/
+	mysqli_close($link);
+	?>
