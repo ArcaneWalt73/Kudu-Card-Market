8496d99cb7258a518fe9af4b47f5563871d1be32
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-23 16:48:47 +0200

    Update testTest.php

--- Tests/testTest.php
+++ Tests/testTest.php
@@ -1,16 +1,25 @@
 <?php
 
+
 class testTest extends PHPUnit\Framework\TestCase
 {
+    
+    /**
+        *@var PD)
+        */
+
+    private $pdo;
+
     public function setUp()
-    { 
-      $this->db->ClearDatabase();
-      $this->session->start();
+    {  
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
     }
 
     public function tearDown()
     {
-        $this->session->destroy();
+        $this->pdo->query("DROP TABLE hello");
     }
 
     public function testTest()
