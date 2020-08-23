1af16e729f495304e520d30f39c805313a23e60a
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-24 01:07:39 +0200

    Update testGetHistory.php

--- Tests/testGetHistory.php
+++ Tests/testGetHistory.php
@@ -10,6 +10,10 @@ class GetHistoryTest extends PHPUnit/Framework/TestCase
     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $this->pdo-query("CREATE TABLE MARKET(MARKET_ID int NOT NULL PRIMARY KEY, IMAGE_URL varchar(30), NAME varchar(30), PRICE int, CATEGORY varchar(30), DESCRIPTION varchar(100))");
     $this->pdo->query("CREATE TABLE PURCHASES(STUDENT_NO int NOT NULL PRIMARY KEY, MARKET_ID int FOREIGN KEY REFERENCES MARKET(MARKET_ID))");
+    
+    #insert dummy values
+    $this->pdo-query("INSERT INTO MARKET VALUES(0, 'img_url','Book',250, 'Hobbies', 'A simple technology for entertainment and knowledge sharing' );");
+    $this->pdo-query("INSERT INTO PURCHASES(1000, 0)");
   }
   
 }
