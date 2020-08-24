cf17a9ea96df0845e26cde9aeb03b86ebbb44967
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-24 18:26:42 +0200

    Update testGetHistory.php

--- Tests/testGetHistory.php
+++ Tests/testGetHistory.php
@@ -1,26 +1,11 @@
 <?php
+
 include('php/getHistory.php');
-class GetHistoryTest extends PHPUnit\Framework\TestCase
-{ 
-  private $pdo;
-  
-  function setUp(): void
+
+class PHPTest extends PHPUnit\Framework\TestCase
+{
+  public funtion testWork()
   {
-    $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $this->pdo-query("CREATE TABLE MARKET(MARKET_ID int NOT NULL PRIMARY KEY, IMAGE_URL varchar(30), NAME varchar(30), PRICE int, CATEGORY varchar(30), DESCRIPTION varchar(100))");
-    $this->pdo->query("CREATE TABLE PURCHASES(STUDENT_NO int NOT NULL PRIMARY KEY, MARKET_ID int FOREIGN KEY REFERENCES MARKET(MARKET_ID))");
-    
-    #insert dummy values
-    $this->pdo-query("INSERT INTO MARKET VALUES(0, 'img_url','Book',250, 'Hobbies', 'A simple technology for entertainment and knowledge sharing' );");
-    $this->pdo-query("INSERT INTO PURCHASES(1000, 0)");
-  }
-  
-  public function testGetHIstory()
-  {
-    #should return null if the user has no history
-    $this->$pdo->assertEquals(null, getUserHistory($link, 100));
+    $this->assertEquals('success', work()); 
   }
 }
-
-?>
