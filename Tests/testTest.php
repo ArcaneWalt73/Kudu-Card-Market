3db0f3c1783107b16c1b9bd1a7b758115094bbff
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-24 18:47:07 +0200

    Update testTest.php

--- Tests/testTest.php
+++ Tests/testTest.php
@@ -1,6 +1,7 @@
 <?php
 
 include('php/test.php');
+include('php/greet.php');
 
 class TestTest extends PHPUnit\Framework\TestCase
 {
@@ -8,6 +9,12 @@ class TestTest extends PHPUnit\Framework\TestCase
     {
         $test = new TestClass();
         $this->assertEquals('success', $test->hello());
+    }
+    
+    public function testGreet()
+    {
+        $res = new Greet();
+        $this->assertEquals('konichiwa', $res->greeting());
     }
 }
 
