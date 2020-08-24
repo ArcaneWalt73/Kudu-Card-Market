9cfb76c1c56e49e9e2785f50ee20247c078e237c
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-24 18:52:54 +0200

    Add files via upload

--- Tests/testTest.php
+++ Tests/testTest.php
@@ -1,20 +1,13 @@
 <?php
 
 include('php/test.php');
-include('php/greet.php');
 
-class TestTest extends PHPUnit\Framework\TestCase
+class PHPTest extends PHPUnit\Framework\TestCase
 {
     public function testTest()
     {
         $test = new TestClass();
         $this->assertEquals('success', $test->hello());
-    }
-    
-    public function testGreet()
-    {
-        $res = new Greet();
-        $this->assertEquals('konichiwa', $res->greeting());
     }
 }
 
