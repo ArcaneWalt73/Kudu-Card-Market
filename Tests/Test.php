67aca7869a268e2ca957d2943d989cf02291e23c
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-25 14:20:57 +0200

    Update Test.php

--- Tests/Test.php
+++ Tests/Test.php
@@ -5,6 +5,21 @@ include('php/test.php');
 
 class PHPTest extends PHPUnit\Framework\TestCase
 { 
+    protected $link;
+    protected function setUp(): void
+    {        
+        $username = "s1965919";
+        $password = "ICTPass1670";
+        $database = "d1965919";
+        
+        $url = "s1965919@lamp.ms.wits.ac.za";
+        $ip = gethostbyname($url);
+        
+        $link = mysqli_connect($ip, $username, $password, $database);
+        $studentNo = mysqli_real_escape_string($link,$_POST["studentNumber"]); 
+        $password1 = mysqli_real_escape_string($link,$_POST["password"]);   
+    }
+    
     public function testTest()
     {
         $test = new TestClass();
