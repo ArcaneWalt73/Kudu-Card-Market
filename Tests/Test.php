85f9de4d07b36423939bfbeb0ea55d8234873527
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-08-25 14:31:12 +0200

    Update Test.php

--- Tests/Test.php
+++ Tests/Test.php
@@ -5,17 +5,6 @@ include('php/test.php');
 
 class PHPTest extends PHPUnit\Framework\TestCase
 { 
-    protected $link;
-    protected function setUp(): void
-    {        
-        $username = "root";
-        $password = "";
-        $database = "d1965919";
-        
-        
-        $link = mysqli_connect("127.0.0.1", $username, $password, $database);   
-    }
-    
     public function testTest()
     {
         $test = new TestClass();
