bead0aea96df288574e363dfdedfbed4b4b97494
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-08-25 16:06:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,48 @@
 <div id="results" style="display: none;">
     <?php
         include('php/getItem.php');
-	#echo $_SESSION['login_user'];_
-	#include('php/session.php');
-	// Insert session checker	
+        #echo $_SESSION['login_user'];_
+        #include('php/session.php');
+        // Insert session checker
     ?>
 </div>
 
 <!DOCTYPE html>
 <html>
     <head>
-	<title>Kudu Mart</title>
+        <title>Kudu Mart</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/css/navbar.css">
-	<link rel="stylesheet" type="text/css" href="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/css/homepage.css">
+        <link rel="stylesheet" type="text/css" href="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/css/navbar_.css">
+        <link rel="stylesheet" type="text/css" href="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/css/homepage.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
-
-
-	<!-- Awesome Font CDN -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
-    </head> 
+        <!-- Awesome Font CDN -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    </head>
 
     <body>
         <div class="navbar">
-            <a class="tablinks active" onclick="main(event,'home')"><i class="fa fa-fw fa-home"></i> Home</a>
-            <a class="tablinks" onclick="main(event,'search')"><i class="fa fa-fw fa-search"></i> Search</a>
+            <a class="tablinks active"><i class="fa fa-fw fa-home"></i> Home</a>
             <a onclick="document.getElementById('login').style.display='block'" style="width:auto;">
                 <i class="fa fa-fw fa-user"></i> Login
             </a>
-	    
-	    <!--div class="dropdown">
-		<button class="dropbtn">Dropdown
-			<i class="fa fa-caret-down"></i>
-		</button>
-		<div class=dropdown-content">
-			<a href="#">Link 1</a>
-			<a href="#">Link 2</a>
-			<a href="#">Link 3</a>
-		</div>
-	    </div-->
-			
-	    <!-- div class="topnav-right">
-              <a class="tablinks"><i class="fas fa-user-alt"></i>Hello, User</a>
+            <div id="searchI" class="search">
+              <form id="searchForm">
+                  <input id="searchInput" type="text"
+                      placeholder=" Search..."
+                      name="searc">
+                  <button id="searchBtn">
+                      <i class="fa fa-search"
+                          style="font-size: 18px;">
+                      </i>
+                  </button>
+              </form>
             </div>
-            <div>
-              <span id="user-text">Hello, User</span>
-            </div-->
         </div>
 
         <div id="home" class="tabcontent"></div>
-        <div id="search" class="tabcontent" style="display: none;">search catagories here</div>
 
         <!--login form-->
         <div id="login" class="modal">
@@ -59,32 +51,32 @@
                 <span onclick="document.getElementById('login').style.display='none'" class="close" title="Close Modal">&times;</span>
                 <img src="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/images/wits_logo.png" alt="Avatar" class="avatar">
               </div>
-          
+
               <div class="container">
                 <input style="border-radius:15px 15px 15px 15px;" type="text" placeholder="Enter Username" name="studentNumber" required>
-          
+
                 <input style="border-radius:15px 15px 15px 15px;" type="password" placeholder="Enter Password" name="password" required>
-                  
+
                 <button style="border-radius:15px 15px 15px 15px;" type="submit">Login</button>
               </div>
-          
+
               <div class="container" style="background-color:#1e376c">
                 <button style="border-radius:15px 15px 15px 15px;" type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
                 <span class="psw"><a href="#" onclick="document.getElementById('register').style.display='block'">Create an account</a></span>
               </div>
             </form>
           </div>
-          
+
           <div id="register" class="modal">
             <form id='registerForm' class="modal-content animate" action="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/php/register.php" method="post" style="background-color:#1e376c">
               <div class="imgcontainer">
                 <span onclick="document.getElementById('register').style.display='none'" class="close" title="Close Modal">&times;</span>
                 <img src="https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/images/wits_logo.png" alt="Avatar" class="avatar">
               </div>
-          
+
               <div class="container">
                 <input style="border-radius:15px 15px 15px 15px;" type="text" placeholder="Enter Username" name="studentNumber" required>
-          
+
                 <input style="border-radius:15px 15px 15px 15px;" type="password" placeholder="Enter Password" name="password" required>
 
                 <input style="border-radius:15px 15px 15px 15px;" type="text" placeholder="First Name" name="fname" required>
@@ -94,10 +86,10 @@
                 <input style="border-radius:15px 15px 15px 15px;" type="email" placeholder="Email" name="email" required>
 
                 <input style="border-radius:15px 15px 15px 15px;" type="tel" placeholder="Contact Number" name="contact" required>
-                  
+
                 <button style="border-radius:15px 15px 15px 15px;" type="submit">Register</button>
               </div>
-          
+
               <div class="container" style="background-color:#1e376c">
                 <button style="border-radius:15px 15px 15px 15px;" type="button" onclick="document.getElementById('register').style.display='none'" class="cancelbtn">Cancel</button>
               </div>
@@ -113,23 +105,6 @@
                     modal.style.display = "none";
                 }
             }
-            /* this function takes in id, which correponds to the tabs. i.e home,search,login
-            home is active by default
-            make the tabs active accordingly 
-            */
-            function main(evt,id){
-                tabcontent = document.getElementsByClassName("tabcontent");
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].style.display = "none";
-                }
-                tablinks = document.getElementsByClassName("tablinks");
-                for (i = 0; i < tablinks.length; i++) {
-                    tablinks[i].className = tablinks[i].className.replace(" active", "");
-                }
-                document.getElementById(id).style.display = "block";
-                evt.currentTarget.className += " active";
-              
-            }
           </script>
           <script>
             var div = document.getElementById("results");
@@ -141,15 +116,15 @@
                     var div1 = document.createElement("div");
                     var div2 = document.createElement("div");
                     div1.setAttribute("class","column");
-		    var string1 = "location.href='./item_page.php?id=";
-		    string1 = string1.concat(myData[i]['MARKET_ID'], "';");
-		    div1.setAttribute("onclick", string1);
+                    var string1 = "location.href='./item_page.php?id=";
+                    string1 = string1.concat(myData[i]['MARKET_ID'], "';");
+                    div1.setAttribute("onclick", string1);
                     div2.setAttribute("class","card");
                     var img = document.createElement("IMG");
-		    img.setAttribute("class", "images");
+                    img.setAttribute("class", "images");
                     img.setAttribute("src", myData[i]["IMAGE_URL"]);
                     img.setAttribute("alt", myData[i]["NAME"]);
-		    img.setAttribute("width","210");
+                    img.setAttribute("width","210");
                     img.setAttribute("height","170");
                     var div3 = document.createElement("div");
                     div3.setAttribute("class","container");
@@ -157,20 +132,68 @@
                     para.innerText = myData[i]["NAME"];
 
                     var priceDiv = document.createElement("div");
-		    priceDiv.setAttribute("class", "container");
-		    var price = document.createElement("P");
-		    price.innerText = "R" + myData[i]["PRICE"];
-		    priceDiv.appendChild(price);
+                    priceDiv.setAttribute("class", "container");
+                    var price = document.createElement("P");
+                    price.innerText = "R" + myData[i]["PRICE"];
+                    priceDiv.appendChild(price);
 
                     div3.appendChild(para);
                     div2.appendChild(img);
                     div2.appendChild(div3);
-		    div2.appendChild(priceDiv);
+                    div2.appendChild(priceDiv);
                     div1.appendChild(div2);
                     home.appendChild(div1);
             }
-          </script>
+            $(document).ready(function(){
+              $('#searchBtn').click(function(){
+                $.post("https://lamp.ms.wits.ac.za/~s1965919/Kudu-Card-Market/php/search_items.php",
+                {
+                  search: document.getElementById("searchInput").value
+                },
+                function(data,status){
+                  var searchData = JSON.parse(data);
+                  sessionStorage.setItem('searchData', JSON.stringify(searchData));
+                });
+              });
+            });
+            if(sessionStorage.length !=0){
+                var retrievedObject = sessionStorage.getItem('searchData');
+                $("#home").empty();
+                myData = JSON.parse(retrievedObject);
+                for (var i=0; i<myData.length; i++){
+                var div1 = document.createElement("div");
+                var div2 = document.createElement("div");
+                div1.setAttribute("class","column");
+                var string1 = "location.href='./item_page.php?id=";
+                string1 = string1.concat(myData[i]['MARKET_ID'], "';");
+                div1.setAttribute("onclick", string1);
+                div2.setAttribute("class","card");
+                var img = document.createElement("IMG");
+                img.setAttribute("class", "images");
+                img.setAttribute("src", myData[i]["IMAGE_URL"]);
+                img.setAttribute("alt", myData[i]["NAME"]);
+                img.setAttribute("width","210");
+                img.setAttribute("height","170");
+                var div3 = document.createElement("div");
+                div3.setAttribute("class","container");
+                var para = document.createElement("P");
+                para.innerText = myData[i]["NAME"];
 
-         
+                var priceDiv = document.createElement("div");
+                priceDiv.setAttribute("class", "container");
+                var price = document.createElement("P");
+                price.innerText = "R" + myData[i]["PRICE"];
+                priceDiv.appendChild(price);
+
+                div3.appendChild(para);
+                div2.appendChild(img);
+                div2.appendChild(div3);
+                div2.appendChild(priceDiv);
+                div1.appendChild(div2);
+                home.appendChild(div1);
+            }
+            sessionStorage.clear();
+          }
+          </script>
     </body>
 </html>
