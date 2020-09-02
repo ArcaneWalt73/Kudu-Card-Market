4261ffa15b7576b49a037a4bd83039dbbbcf828e
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-09-03 00:18:09 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,22 +1,44 @@
 <?php
+session_start();
+class login{
+    public $studentNo;
+    public $password1;
+    private $database;
+    public function __construct($stud, $pass){
+        $this->studentNo = $stud;
+        $this->password1 = $pass;
+        $this->database = new Database;
+    }
 
-$studentNo = mysqli_real_escape_string($link,$_POST["studentNumber"]);
-$password1 = mysqli_real_escape_string($link,$_POST["password"]);
+    public function getAllTasks() {
+        $this->database->query('select PASSWORD from STUDENTS where STUDENT_NO='.$this->studentNo);
+        $results = $this->database->resultSet();
+        return $results;
+    }
 
-function doLogin($stdNo, $pass){
-    $username = "s1965919";
-    $password = "ICTPass1670";
-    $database = "d1965919";
-    $link = mysqli_connect('127.0.0.1', $username, $password, $database);
-    session_start();
-    if ($result = mysqli_query($link, "select PASSWORD from STUDENTS where STUDENT_NO='$stdNo';")){
-        $hashed = $result->fetch_assoc()['PASSWORD'];
-        if(!empty($pass) && password_verify($pass,$hashed)){
-            $_SESSION['login_user'] = $stdNo;
+    public function doLogin(){
+        if($result = $this->getAllTasks()){
+            $hashed = $result[0]["PASSWORD"];
+            $pass = $this->password1;
+            if(!empty($pass) && password_verify($pass,$hashed)){
+                $_SESSION['login_user'] = $this->studentNo;
+                return true; //success
+            }
+            else if(!empty($pass) && !password_verify($pass,$hashed)){
+                return false; //wrong password
+            }
         }
-        header("location: ../homepage.php");
-        mysqli_close($link);
+        return false; //the username does not exist
+
     }
 }
-doLogin($studentNo, $password1);
+
+/*$user = new login($_POST["studentNumber"],$_POST["password"]);
+$var = $user->doLogin();
+if($user->doLogin()){
+    header('location: ../homepage.php');
+}
+else{
+    header('location: ../index.php');
+}*/
 ?>
