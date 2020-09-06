db8c00084090168d41b62e41dce0a02da6bf1463
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-07 01:39:09 +0200

    Update secondTest.php

--- Tests/secondTest.php
+++ Tests/secondTest.php
@@ -3,6 +3,18 @@ include('php/test.php');
 
 class secondTest extends PHPUnit\Framework\TestCase
 { 
+    $username = "root";
+    $password = "toor";
+    $database = "d1965919";
+    $link = mysqli_connect("127.0.0.1", $username, $password,$database);
+    
+    protected function setUp(): void
+    {
+           $sql = "create table DUMMY_T(id int,words varchar)";
+           $link->query($sql);
+           
+    }
+    
     public function testGoodbye()
     {
         $this->assertEquals('success', goodbye());
