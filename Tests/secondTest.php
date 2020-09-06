c8c13732ed3c4dac82e10a5617b92a1638c20a97
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-07 01:43:25 +0200

    Update secondTest.php

--- Tests/secondTest.php
+++ Tests/secondTest.php
@@ -3,15 +3,17 @@ include('php/test.php');
 
 class secondTest extends PHPUnit\Framework\TestCase
 { 
-    $username = "root";
-    $password = "toor";
-    $database = "d1965919";
-    $link = mysqli_connect("127.0.0.1", $username, $password,$database);
     
     protected function setUp(): void
     {
-           $sql = "create table DUMMY_T(id int,words varchar)";
-           $link->query($sql);
+            $username = "root";
+            $password = "toor";
+            $database = "d1965919";
+            $link = mysqli_connect("127.0.0.1", $username, $password,$database);
+    
+        
+            $sql = "create table DUMMY_T(id int,words varchar)";
+            $link->query($sql);
            
     }
     
