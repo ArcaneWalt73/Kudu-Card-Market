c415cbc481ec1b6a67a5dcd7ccdd6193a02906d3
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-07 03:34:30 +0200

    Update test2.php

--- php/test2.php
+++ php/test2.php
@@ -2,11 +2,21 @@
 
   function saySomething($link)
   { 
-     $sql = "select words from DUMMY_T where id=0";
-     $result = mysqli_query( $link, $sql );
-     $row = $result->mysqli_fetch_assoc();
-     
-     return $row;     
+     #$sql = "select words from DUMMY_T where id=0";
+     #$result = mysqli_query( $link, $sql );
+     #$row = $result->mysqli_fetch_assoc();
+
+      $output = array();
+      if ($mResult = mysqli_query($link, "select words from DUMMY_T where id=0;")) 
+      {
+				while ($row = $mResult->fetch_assoc())
+        {
+					$output = $row;
+        }
+      }
+    
+    
+     return output[0];     
   }
 
   function goodbye()
