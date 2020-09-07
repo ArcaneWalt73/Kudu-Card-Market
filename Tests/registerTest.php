d777c8cc64b43ee3846c518632d4ce55e6183271
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-09-07 20:16:08 +0200

    Update registerTest.php

--- Tests/registerTest.php
+++ Tests/registerTest.php
@@ -18,9 +18,6 @@
             $task = new register('11233','kag','mos','123','ema22il222@gmail.com','0981712672');
             $this->assertTrue($task->doRegister());
             //test for failure
-            $task = $this->getMockBuilder('register')->enableOriginalConstructor()
-                ->setConstructorArgs(array('11233','kag','mos','123','ema22il222@gmail.com','0981712672'))->getMock();
-            $task->method('getAllTasks')->will($this->returnValue(false));
             $this->assertEquals(false,$task->doRegister());
         }
 }
