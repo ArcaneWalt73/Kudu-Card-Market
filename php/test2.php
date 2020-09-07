2a7f71f2ab642adcfe54e806215260daa9f2cb22
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-07 02:25:18 +0200

    Update test2.php

--- php/test2.php
+++ php/test2.php
@@ -1,4 +1,11 @@
 <?php
+
+  function saySomething($link)
+  { 
+     $sql = "select words from DUMMY_T where id=0";
+     return $link->query($sql);     
+  }
+
   function goodbye()
   {
     return "success"; 
