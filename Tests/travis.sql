a3c09e1c1d022040b31fc6d1799001789b465a9d
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-09-07 16:00:26 +0200

    Create travis.sql

---
+++ Tests/travis.sql
@@ -0,0 +1,19 @@
+# Create Testuser
+#CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+#GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+
+# Create DB
+CREATE DATABASE IF NOT EXISTS `d1965919` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `d1965919`;
+
+# Create Table
+CREATE TABLE IF NOT EXISTS `student` (
+  `student_no` varchar(20) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`student_no`);
+
+# Add Data
+INSERT INTO `student`(`student_no`,`password`) values('1234','123')
