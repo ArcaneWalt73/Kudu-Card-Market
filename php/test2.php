ff735f09485868433bf4618ca7cc0c68fb3a059d
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-07 03:15:29 +0200

    Update test2.php

--- php/test2.php
+++ php/test2.php
@@ -3,7 +3,7 @@
   function saySomething($link)
   { 
      $sql = "select words from DUMMY_T where id=0";
-     $result = mysqli_query( $sql );
+     $result = mysqli_query( $link, $sql );
      $row = mysqli_fetch_assoc($result);
       $data = $row['words'];
      
