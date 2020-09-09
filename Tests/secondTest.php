d30fc3e6b47a6fc3a86d7fc99e9f366bed1c1bc4
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-09-09 16:20:59 +0200

    Update secondTest.php

--- Tests/secondTest.php
+++ Tests/secondTest.php
@@ -22,7 +22,7 @@ class secondTest extends PHPUnit\Framework\TestCase
         $sql = "insert into DUMMY_T(id, words) values(0, 'yokatta');";
         $link->query($sql);
         
-        $result = $link->query("select * from DUMMY_T;");
+        $result = $link->query("desc;");
         
         echo "________RESULT=:   ".$result;
             
