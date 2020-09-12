652fda8fb1ca5b02e071137d861894b9a14f4140
Ch35tEr <48945081+Ch35tEr@users.noreply.github.com>
2020-09-12 20:57:44 +0200

    Updated helperFunctions.php

--- php/helperFunctions.php
+++ php/helperFunctions.php
@@ -1,4 +1,5 @@
 <?php
+require 'Database.phphh';
 /*
  * Function to retrieve an item's data when given the item's id
  */
@@ -47,23 +48,69 @@ function buyItem($link, $id, $studentID) {
 
 }
 
+class Database1 {
+
+    private $host = '127.0.0.1';
+    private $user = 'root';
+    private $pass = '';
+    private $dbname = 'd1965919';
+
+    private $db;
+    private $stmt;
+    private $testPassword;
+    private $testUser;
+    private $error;
+
+    public function __construct(){
+        // Set DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        $this->db = new PDO($dsn, $this->user, $this->pass, $options);
+    }
+
+    public function query($sql){
+        try{
+            $this->stmt = $this->db->exec($sql);
+        }catch(PDOException $e){
+            $this->stmt = -1;
+        }
+    }
+
+    public function resultSet(){
+        return $this->stmt;
+
+    }
+}
+
 class HelperFunctions {
 	private $link;
+	private $database;
 
 	function __construct() {
-		$username = "s1965919";
-		$password = "ICTPass1670";
-		$database = "d1965919";
-		$link = mysqli_connect("127.0.0.1", $username, $password, $database);
+		$this->database = new Database1;
 	}
 	
 	/*
 	 * Function to retrieve an item's data when given the item's id
 	 */
 	function getItemInfo($id){
-		#echo $id;
-       		$response = array();
-		if ($result = mysqli_query($link, "select * from MARKET_NEW where MARKET_ID='$id'")) {
+		//$id = mysqli_c$id;
+       	$response = array();
+       	$this->database->query('SELECT * FROM MARKET_NEW WHERE MARKET_ID='.$id);
+       	if($result = $this->database->resultSet()) {
+       		$this->database->query('SELECT RATING FROM RATINGS WHERE MARKET_ID='.$id);
+       		$rating = $this->database->resultSet()
+       		echo $result;
+       		echo $rating;
+       		$response['ERROR'] = false;
+       		return $response;
+       	}
+       	$response['ERROR'] = true;
+
+		/*if ($result = mysqli_query($link, "select * from MARKET_NEW where MARKET_ID='$id'")) {
 			$rating = mysqli_query($link, "select RATING from RATINGS where MARKET_ID='$id'");
 			$data = $rating->fetch_assoc();
 			$dataResult = $result->fetch_assoc();
@@ -79,14 +126,25 @@ class HelperFunctions {
 			return $response;
 		}
        		$response['ERROR'] = true;
+       		*/
 	}
 
 
 	/*
 	 * Function to buy an Item
 	 */
-	function buyItem($id, $studentID, $password) {
-		if ($result = mysqli_query($link, "select KUDU_BUCKS, PASSWORD from STUDENTS where STUDENT_NO='$studentNo';")){
+	function buyItem($id, $studentNo, $password) {
+		$this->database->query('SELECT KUDU_BUCKS, PASSWORD FROM STUDENTS WHERE STUDENT_NO='.$studentNo);
+		if ($result = $this->database->resultSet()) {
+			echo $result;
+			$row = $result->fetchAll(PDO::FETCH_ASSOC);
+			echo $row['PASSWORD'];
+			echo $row['KUDU_BUCKS'];
+		}
+
+
+
+		/*if ($result = mysqli_query($link, "select KUDU_BUCKS, PASSWORD from STUDENTS where STUDENT_NO='$studentNo';")){
 			$row = mysqli_fetch_array($result);
 			if ($row['PASSWORD'])
 			if ($row['KUDU_BUCKS'] >= $amount) {
@@ -103,7 +161,7 @@ class HelperFunctions {
 			}
 			else
 				echo "false 3";
-		}
+		}*/
 	}
 
 	function removeItem($id) {
