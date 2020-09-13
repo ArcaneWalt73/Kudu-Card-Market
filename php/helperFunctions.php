c021eacf0fd63d482ab75ab23c375da8edae1dfc
Ch35tEr <48945081+Ch35tEr@users.noreply.github.com>
2020-09-14 01:55:34 +0200

    Update helperFunctions.php

--- php/helperFunctions.php
+++ php/helperFunctions.php
@@ -106,95 +106,80 @@ class HelperFunctions {
 	function getItemInfo($id){
 		//$id = mysqli_c$id;
        	$response = array();
-	echo "checkpoint : 1</br>";
-       	$this->database->query('SELECT * FROM `MARKET_NEW` WHERE MARKET_ID='.$id);
-	
-       	$result = $this->database->resultSet();
-	if ($result !== -1) {
+		echo "checkpoint : 1</br>";
+	    $this->database->query('SELECT * FROM `MARKET_NEW` WHERE MARKET_ID='.$id);
+		
+	    $result = $this->database->resultSet();
+		if ($result !== -1) {
        		$this->database->query('SELECT * FROM RATINGS WHERE MARKET_ID='.$id);
        		$rating = $this->database->resultSet();
-		echo "Checkpoint : 2</br>";
+			echo "Checkpoint : 2</br>";
        		$this->database->exec('SELECT * FROM RATINGS WHERE MARKET_ID='.$id);
-		echo $this->database->resultSet();
-		#echo json_encode($result->fetch());
-		echo json_encode($rating->fetch(PDO::FETCH_ASSOC));
+			echo $this->database->resultSet();
+			#echo json_encode($result->fetch());
+			echo json_encode($rating->fetch(PDO::FETCH_ASSOC));
 
-		$data = $rating->fetch();
-		$dataResult = $result->fetch();
-		//response array
-		$response['ID'] = $dataResult['MARKET_ID'];
-		$response['NAME'] = $dataResult['NAME'];
-		$response['URL'] = $dataResult['IMAGE_URL'];
-		$response['PRICE'] = $dataResult['PRICE'];
-		$response['CATEGORY'] = $dataResult['CATEGORY'];
-		$response['DESCRIPTION'] = $dataResult['DESCRIPTION'];
-		$response['RATING'] = $data['RATING'];
-		$response['REVIEWS'] = $data['REVIEWS'];
-	        $response['ERROR'] = false;
-
-       		#echo $r1['MARKET_ID']."</br>";
-       		#echo $r2['RATING']."</br>";
-		
-       		$response['ERROR'] = false;
-       	}
-	else
-       		$response['ERROR'] = true;
-	echo "Checkpoint : 3</br>";
-	return $response;
-
-		/*if ($result = mysqli_query($link, "select * from MARKET_NEW where MARKET_ID='$id'")) {
-			$rating = mysqli_query($link, "select RATING from RATINGS where MARKET_ID='$id'");
-			$data = $rating->fetch_assoc();
-			$dataResult = $result->fetch_assoc();
-
-		        //response array
-       		 	$response['ID'] = $dataResult['MARKET_ID'];
+			$data = $rating->fetch();
+			$dataResult = $result->fetch();
+			//response array
+			$response['ID'] = $dataResult['MARKET_ID'];
 			$response['NAME'] = $dataResult['NAME'];
 			$response['URL'] = $dataResult['IMAGE_URL'];
 			$response['PRICE'] = $dataResult['PRICE'];
 			$response['CATEGORY'] = $dataResult['CATEGORY'];
 			$response['DESCRIPTION'] = $dataResult['DESCRIPTION'];
-	        	$response['ERROR'] = false;
-			return $response;
-		}
-       		$response['ERROR'] = true;
-       		*/
+			$response['RATING'] = $data['RATING'];
+			$response['REVIEWS'] = $data['REVIEWS'];
+		        $response['ERROR'] = false;
+
+	       		#echo $r1['MARKET_ID']."</br>";
+	       		#echo $r2['RATING']."</br>";
+			
+	       		$response['ERROR'] = false;
+	       	}
+		else
+	       		$response['ERROR'] = true;
+		echo "Checkpoint : 3</br>";
+		return $response;
 	}
 
 
 	/*
 	 * Function to buy an Item
+	 * Returns :
+	 *		0 - Success
+	 *		1 - Failed to record transation
+	 *		2 - Failed to update new amount
+	 *		3 - Insuffient Credit
 	 */
-	function buyItem($id, $studentNo, $password) {
-		$this->database->query('SELECT KUDU_BUCKS, PASSWORD FROM STUDENTS WHERE STUDENT_NO='.$studentNo);
+	function buyItem($id, $studentNo) {
+		$this->database->query('SELECT KUDU_BUCKS FROM STUDENTS WHERE STUDENT_NO='.$studentNo);
 		$result = $this->database->resultSet()
-		if ($result !== -1) {
-			echo $result;
-			$row = $result->fetchAll(PDO::FETCH_ASSOC);
-			echo $row['PASSWORD'];
-			echo $row['KUDU_BUCKS'];
-		}
+		$this->database->query('SELECT * FROM MARKET_NEW WHERE MARKET_ID='.$id);
+		$itemResult = $this->database->resultSet();
+		if ($result !== -1 && $itemResult !== -1) {
+			$row = $result->fetch(PDO::FETCH_ASSOC);
+			$col = $itemResult->fetch(PDO::FETCH_ASSOC);
+			$kudu = $row['KUDU_BUCKS'];
 
-
-
-		/*if ($result = mysqli_query($link, "select KUDU_BUCKS, PASSWORD from STUDENTS where STUDENT_NO='$studentNo';")){
-			$row = mysqli_fetch_array($result);
-			if ($row['PASSWORD'])
-			if ($row['KUDU_BUCKS'] >= $amount) {
-				$current_Amount = $row['KUDU_BUCKS'] - $amount; // the amount after payments
-				if(mysqli_query($link,"update STUDENTS set KUDU_BUCKS='$current_Amount' where STUDENT_NO='$studentNo';")){
+			$amount = $col['PRICE'];
+			if ($kudu >= $amount) {
+				$current_Amount = $kudu - $amount; // the amount after payments
+				$this->database->exec('UPDATE STUDENTS SET KUDU_BUCKS='.$current_Amount.'WHERE STUDENT_NO='.$studentNo);
+				if($this->database->resultSet() !== 0){
 					$date = date("Y-m-d");
-					if(mysqli_query($link, "insert into PURCHASES(STUDENT_NO, MARKET_ID, PURCHASE_DATE) values('$studentNo', '$itemID', CURRENT_DATE);"))
-						echo json_encode($current_Amount);
-					else 
-						echo "false 1";
+					$this->database->exec('INSERT INTO PURCHASES(STUDENT_NO, MARKET_ID, PURCHASE_DATE) VALUES('.$studentNo.', '.$id.', CURRENT_DATE)');
+					if($this->database->resultSet() !== 0)
+						return 0;
+					else
+						return 1
 				}
 				else
-					echo "false 2";
+					return 2
 			}
 			else
-				echo "false 3";
-		}*/
+				return 3
+		}
 	}
 
 	function removeItem($id) {
