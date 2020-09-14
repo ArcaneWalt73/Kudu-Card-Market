85916352ac06aa659e46449aec38ac8b23557930
Tshegofatso-Mohlala <61188084+Tshegofatso-Mohlala@users.noreply.github.com>
2020-09-14 19:36:22 +0200

    had concurrency issues

--- php/Username2.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $username = "s1965919";
-        $password = "ICTPass1670";
-        $database = "d1965919";
-        $link = mysqli_connect('127.0.0.1', $username, $password, $database);
-        if($session_sql = mysqli_query($link,"delete from loggedIn ")){
-                echo 'Done';
-        }
-?>
