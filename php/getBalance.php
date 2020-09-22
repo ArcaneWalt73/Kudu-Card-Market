69e7ab0d794f190977b16f2b461ae5e301c09147
Ch35tEr <1965919@students.wits.ac.za>
2020-09-22 16:34:33 +0200

    added header to the session pages

---
+++ php/getBalance.php
@@ -0,0 +1,14 @@
+<?php
+   //check if a specific logged in successfully
+  $user_check = $_SESSION['login_user'];
+  $username = "s1965919";
+  $password = "ICTPass1670";
+  $database = "d1965919";
+  $link = mysqli_connect('127.0.0.1', $username, $password, $database);
+
+  $session_sql = mysqli_query($link,"SELECT KUDU_BUCKS FROM `STUDENTS` WHERE STUDENT_NO = '$user_check' ");
+
+  $row = mysqli_fetch_array($session_sql,MYSQLI_ASSOC);
+
+  $login_balance = $row['KUDU_BUCKS'];
+?>
