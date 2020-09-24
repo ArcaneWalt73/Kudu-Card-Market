b9716b72e57ce86816ff7ad7c59b14fad5736168
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-09-24 18:23:17 +0200

    Update gethistoryTest.php

--- Tests/gethistoryTest.php
+++ Tests/gethistoryTest.php
@@ -13,7 +13,7 @@ class gethistoryTest extends PHPUnit\Framework\TestCase
     $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    //create the PURCHASES table
+    /*create the PURCHASES table
     $this->pdo->query(
       "CREATE TABLE IF NOT EXISTS PURCHASES (
        STUDENT_NO varchar(20) ,
@@ -43,7 +43,7 @@ class gethistoryTest extends PHPUnit\Framework\TestCase
       (11,'https://lamp.ms.wits.ac.za/~s1965919/uploads/11.png','Asus Laptop 15',8999.99,'Electronics','Whether for work or play, ASUS M509 is the entry-level laptop that delivers powerful performance and immersive visuals. Its NanoEdge display boasts wide 178? viewing angles and a matte anti-glare coating for a truly engaging experience. Inside, it?s powered by up to AMD Ryzen? processor with 8GB RAM andf a fast 512GB SSD gives you the perfect combination of large storage capacity and fast data read/write speeds.'),
       (12,'https://lamp.ms.wits.ac.za/~s1965919/uploads/12.jpeg','Lenovo ThinkPad UltraSlim Supe',1193.62,'Electronics','PRODUCT DETAILS\n\nBrand:Lenovo\n\nFormat:DVD\n\nType:External\n\nConnection:USB\n\nLenovo ThinkPad UltraSlim USB DVD Burner - Disk drive - DVD?RW (?R DL) / DVD-RAM - SuperSpeed USB 3.0 - external - CRU - for ThinkCentre M83; ThinkPad E440; E540; X1 Carbon'),
       (13,'https://lamp.ms.wits.ac.za/~s1965919/uploads/13.jpeg','Acer Nitro 5',9999.99,'Electronics','PRODUCT DETAILS\n\nOperating system:Windows OS\n\nScreen size:15.6 in\n\nInstalled memory:3 GB RAM\n\nGraphics processor:NVIDIA GPU\n\nExplore and enjoy a new and more immersive level of gaming with the Nitro 5?s full HD display and powerful gaming tech.');"
-    );
+    );*/
     
     global $pdo;
     $pdo = $this->pdo;
@@ -53,7 +53,7 @@ class gethistoryTest extends PHPUnit\Framework\TestCase
   {
       global $pdo;
      //test to see that the MarketId of the value in the first row is 18
-     $this->assertEquals(18,getPurchases($pdo,'7777777'));
+     $this->assertEquals(17,getPurchases($pdo,'1234'));
   }
   
   public function testgetItemDetails()
