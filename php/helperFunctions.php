822fe232a30b86f0392637b05a1cd226dede09a2
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-10-01 23:39:12 +0200

    Update helperFunctions.php

--- php/helperFunctions.php
+++ php/helperFunctions.php
@@ -1,10 +1,10 @@
 <?php
 
-class Database1 {
+class Database2 {
 
     private $host = '127.0.0.1';
-    private $user = 's1965919';
-    private $pass = 'ICTPass1670';
+    private $user = 'root';
+    private $pass = '';
     private $dbname = 'd1965919';
 
     private $db;
