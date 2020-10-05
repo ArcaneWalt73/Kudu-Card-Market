f9a3ed1577364f7e05a84a005ce449d7a6dbe3a4
ArcaneWalt73 <49840096+ArcaneWalt73@users.noreply.github.com>
2020-10-05 14:51:54 +0200

    Update helperFunctionsTest.php

--- Tests/helperFunctionsTest.php
+++ Tests/helperFunctionsTest.php
@@ -84,5 +84,11 @@ require_once 'php/helperFunctions.php';
             $helper = new HelperFunctions();
             $this->assertEquals(17, $helper->getPurchHistory(1234)[0]['MARKET_ID']);
         }
+      
+        public function testGetCartItems()
+        {
+            $helper = new HelperFunctions();
+            $this->assertEquals(17, $helper->getCartItems(1234)[0]['MARKET_ID']);
+        }
         
 }
