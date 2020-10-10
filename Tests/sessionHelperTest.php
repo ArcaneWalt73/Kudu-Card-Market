7564bb589fa20b9950ddfd048e728f115150a415
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-10-11 00:47:07 +0200

    Create sessionHelperTest.php

---
+++ Tests/sessionHelperTest.php
@@ -0,0 +1,32 @@
+<?php
+require 'php/sessionHelper.php';
+
+  class sessionHelperTest extends PHPUnit\Framework\TestCase {
+      public function testGetUserSession(){
+          $return_value = Array('name'=>'davis',
+                                'code'=>0);
+          $_SESSION['login_user'] = 'davis';
+          $this->assertEquals($return_value,getUserSession());
+          unset($_SESSION['login_user']);
+          $return_value1 = Array('code'=>1);
+          $this->assertEquals($return_value1,getUserSession());
+      }
+
+      public function testCheckUserSession(){
+          $user = 'davis';
+          $this->assertEquals(1,checkUserSession($user));
+          $_SESSION['login_user'] = 'davis';
+          $this->assertEquals(2,checkUserSession('wrongUser'));
+          $this->assertEquals(0,checkUserSession($user));
+      }
+
+      public function testUpdateUserSession(){
+          unset($_SESSION['login_user']);
+          $this->assertEquals(0,updateSession('davis'));
+          $_SESSION['login_user'] = 'davis';
+          $this->assertEquals(2,updateSession('wrongUser'));
+          $user = 'davis';
+          $this->assertEquals(1,updateSession($user));
+      }
+
+  }
