46c62f2552ef39623916ff9cd173bdc861fe486b
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-10-11 02:05:54 +0200

    Update removeItemFromCart.php

--- php/removeItemFromCart.php
+++ php/removeItemFromCart.php
@@ -1,32 +1,21 @@
 <?php
-
+session_start();
 include("helperFunctions.php");
 
-session_start();
-$username = "s1965919";
-$password = "ICTPass1670";
-$database = "d1965919";
-$link = mysqli_connect("127.0.0.1", $username, $password, $database);
-
-if(isset($_POST['MARKET_ID']))
-{
-	$id = $_POST['MARKET_ID'];
-	$output = array(); //stores all the items from different catogories
+function removeItemFromCart($id){
+	$username = "root";
+	$password = "";
+	$database = "d1965919";
+	$link = mysqli_connect("127.0.0.1", $username, $password, $database);
 
 	$studentNo = $_SESSION['login_user'];
 
 	$helper = new HelperFunctions;
 
-	if($helper->removeFromCart($id, $studentNo) === 0)
-	{
-		echo "success";
-	}else
-	{
-		echo "failed";
+	if($helper->removeFromCart($id, $studentNo) === 0){
+			return "success";
 	}
-}else
-{
-	echo "didn't receive id";
 }
-
-
+/*usage of this function
+*if(isset($_POST["MARKET_ID"])) echo removeItemFromCart($_POST["MARKET_ID"]);
+*/
