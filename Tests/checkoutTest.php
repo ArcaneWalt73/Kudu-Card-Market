b1be09fe92938a920a375946b1ed704414eb56f6
davis-hub <61417073+davis-hub@users.noreply.github.com>
2020-10-12 02:31:48 +0200

    Create checkoutTest.php

---
+++ Tests/checkoutTest.php
@@ -0,0 +1,11 @@
+<?php
+require 'php/checkout.php';
+
+  class checkoutTest extends PHPUnit\Framework\TestCase {
+      function testCheckOut(){
+          $_SESSION['login_user'] = '1234';
+          $_POST['NEW_BAL'] = 3999.99;
+          $result[] = "updated balace";
+          $this->assertEquals(json_encode($result),checkOut());
+      }
+  }
